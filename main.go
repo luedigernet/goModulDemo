cdf7972d4f684a92f5f5bf93f7215d0d57cc26d0
Reinhard Luediger <reinhard.luediger@metronom.com>
2020-01-25 10:41:48 +0100

    Changed API to v2

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package go_module_a
 
 import "fmt"
 
-func Demo(name string) string {
-	return fmt.Sprintf("Name %s", name)
+func Demo(name string, age int) string {
+	return fmt.Sprintf("Name %s,%d", name, age)
 }
